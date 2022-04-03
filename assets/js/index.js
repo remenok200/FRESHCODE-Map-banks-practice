74b2d48c5d5f3405d773ff77da9baf2ecd2f4bf4
remenok200 <remenok9@gmail.com>
2022-04-03 16:18:40 +0300

    add advanced lvl

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,16 +1,19 @@
 const client3 = {
   fullName: "Ratushnyi Roman",
   clientLevel: "platinum",
+  money: 150000,
 };
 
 const client2 = {
   fullName: "Ivanov Ivan",
   clientLevel: "pro",
+  money: 120000,
 };
 
 const client1 = {
   fullName: "Petrov Petr",
   clientLevel: "basic",
+  money: 14000,
 };
 
 const bank = {
@@ -36,8 +39,21 @@ map.set("platinum", bank.clientLevels.platinum);
 function costCalculate(client, cost) {
   if (map.has(client.clientLevel)) {
     const percent = map.get(client.clientLevel);
-    return cost - (cost * percent.discount) / 100;
+    return (cost - (cost * percent.discount) / 100).toFixed(2);
   } else {
     return cost;
+  }
+}
+
+function buy(client, cost) {
+  if (client.money >= costCalculate(client, cost)) {
+    client.money -= costCalculate(client, cost);
+    return `Покупка успешна! На Вашем счету осталось ${client.money.toFixed(
+      2
+    )}`;
+  } else {
+    return `Ошибка! Вам не хватает ${Math.abs(
+      client.money - costCalculate(client, cost)
+    ).toFixed(2)} для покупки этого товара! =(`;
   }
 }
